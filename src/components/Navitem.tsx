e7023051dd0b9f85bca69fda8decc9505131f7ad
Ajaya Bahadur K C <kshettryaj93@gmail.com>
2023-11-03 15:58:50 +1100

    Navbar and logo changed

--- src/components/Navitem.tsx
+++ src/components/Navitem.tsx
@@ -8,11 +8,22 @@ const Navitem = ({ name, link }: { name: string; link: string }) => {
       <NavLink
         to={link}
         style={({ isActive }) =>
-          isActive ? { color: "hsl(13, 100%, 50%)" } : {}
+          isActive
+            ? {
+                backgroundImage:
+                  "linear-gradient(98.37deg, #f89e00 0.99%, #da2f68 100%)",
+                WebkitBackgroundClip: "text",
+                WebkitTextFillColor: "transparent",
+                fontWeight: ''
+              }
+            : {}
         }
-        className="hover:text-primary"
+        // className="hover:text-primary "
+        className={({ isActive }) =>
+          isActive ? "custom-gradient-text" : "hover:custom-gradient-text"
+        }
       >
-        {name}
+        <span className="font-bold">{name}</span>
       </NavLink>
     </li>
     // <li>
--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -18,9 +18,9 @@ const SearchBar = () => {
         placeholder="search"
         onChange={(e) => setSearchValue(e.target.value)}
         onKeyDown={searchHandler}
-        className="relative left-0 top-0 w-full h-full text-white bg-transparent border border-[rgba(255, 255, 255, 0,5)] outline-none rounded-sm  pl-12 pr-3 backdrop-blur-md placeholder:text-white"
+        className="relative left-0 top-0 w-full h-full text-white bg-transparent border border-[rgba(255, 255, 255, 0,5)] outline-none rounded-md  pl-9 pr-3 backdrop-blur-md placeholder:gray-300"
       />
-      <Search className="absolute top-0 bottom-0 m-auto left-3 pr-3 pt-0 pb-0  text-white border-r border-white w-[30px]"></Search>
+      <Search className="absolute top-0 bottom-0 m-auto left-3 pr-3 pt-0 pb-0  text-gray-300  w-[30px]"></Search>
     </div>
   );
 };
